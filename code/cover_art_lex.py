d8bd26865662b5870848f7b45a87e3d457b0d931
Derek Wacks <djw2157@columbia.edu>
2024-01-04 17:24:24 -0500

    Update cover_art_lex.py
    Removed excess spaces

--- code/cover_art_lex.py
+++ code/cover_art_lex.py
@@ -15,7 +15,6 @@ import logging
 import random
 from botocore.config import Config
 import json
-
 import matplotlib.colors as colors
 from colorthief import ColorThief
 from urllib.request import urlopen
@@ -24,16 +23,11 @@ import ssl
 import requests
 
 
-
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-
 access_key_id = os.environ['aws_access_key_id']
 secret_access_key = os.environ['secretkey']
-
 unsplash_access_key = os.environ['unsplash_access_key']
-
-
 
 my_config = Config(
     region_name='us-west-2',
@@ -44,7 +38,6 @@ my_config = Config(
     }
 )
 
-
 # Setting up SQS
 sqs = boto3.resource('sqs', aws_access_key_id=access_key_id,
                      aws_secret_access_key=secret_access_key, region_name='us-west-2')
@@ -52,43 +45,32 @@ sqs = boto3.resource('sqs', aws_access_key_id=access_key_id,
 queue_name = "music_rec_queue1"
 queue = sqs.get_queue_by_name(QueueName=queue_name)
 print(queue.url)
-
 # Setting up S3
 s3 = boto3.client('s3', aws_access_key_id=access_key_id,
                   aws_secret_access_key=secret_access_key,
                   region_name="us-west-2")
-                  
-
 # Setting up cognito
 cognito = boto3.client('cognito-idp', aws_access_key_id=access_key_id,
                      aws_secret_access_key=secret_access_key, region_name='us-west-2')
                      
-                     
-
 LEXUSERID = ""
-# SWITCHOVER
 username = ""
 user_email = ""
-    
 
 lambda_client = boto3.client('lambda', aws_access_key_id=access_key_id,
                   aws_secret_access_key=secret_access_key,
                   region_name="us-west-2")
-                  
-                  
-                  
+
+
 def send_to_next_lambda(data):
     print("DATA", data)
     d = {
         "data" : data
     }
-
     js = json.dumps(d)
     print("json", js)
-    
     data = bytes(js, encoding='utf-8')
     print("data sending:", data)
-    
     resp = lambda_client.invoke(
         FunctionName='cover_art_lex2',
         InvocationType='Event', # async (don't wait for response)
@@ -98,10 +80,10 @@ def send_to_next_lambda(data):
     resp = json.loads(json.dumps(resp, default=str))
     return resp
     
-    
 
 def get_slots(intent_request):
     return intent_request['currentIntent']['slots']
+
 
 def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message):
     return {
@@ -115,6 +97,7 @@ def elicit_slot(session_attributes, intent_name, slots, slot_to_elicit, message)
         }
     }
 
+
 def close(session_attributes, fulfillment_state, message):
     response = {
         'sessionAttributes': session_attributes,
@@ -124,7 +107,6 @@ def close(session_attributes, fulfillment_state, message):
             'message': message
         }
     }
-
     return response
 
 
@@ -160,35 +142,29 @@ def build_validation_result(is_valid, violated_slot, message_content):
 
 
 def validate_rec_request(fav_color, animal, genre):
-
     genre_options = ["rock", "hiphop", "country", "alternative", "pop"]
     if genre is not None: 
         if genre.lower() not in genre_options:
             return build_validation_result(False, 
                                            'genre',
                                            '{} is not an option, please choose: rock, hiphop, alternative, pop, or country'.format(genre))
-      
     if fav_color is not None:                                    
         try:
             col_1 = colors.to_rgb(fav_color.lower())  # Given as string
         except: # color string didn't work
             return build_validation_result(False, 
                                'color',
-                               '{} is not an option, please choose a valid color'.format(fav_color))
-                               
+                               '{} is not an option, please choose a valid color'.format(fav_color))           
     if animal is not None: 
         # Check animal against hardcoded list
         if animal_check(animal) == False:
             return build_validation_result(False, 
                    'animal',
                    'Could not validate {}, please choose a different animal'.format(animal))
-        
     return build_validation_result(True, None, None)
 
 
 """ --- Functions that control the bot's behavior --- """
-
-
 def animal_check(animal):
     list_of_animals = {'wallaby', 'weasel', 'crow', 'wildcat', 'alligator', 'pigeon', 'louse', 'mallard', 'ibis', 'jackal', 'monkey', 'antelope', 'mole', 'jay', 'ape', 'ram', 'crab', 'yak', 'lapwing', 'squid', 'caribou', 'goldfish', 'goat', 'clam', 'ibex', 'chicken', 'sand dollar', 'sandpiper', 'spider', 'grouse', 'peafowl', 'jellyfish', 'ant', 'turtle', 'cassowary', 'woodpecker', 'dove', 'deer', 'cockroach', 'beaver', 'albatross', 'dugong', 'dunlin', 'llama', 'meerkat', 'walrus', 'horse', 'mink', 'wasp', 'hedgehog', 'grasshopper', 'goose', 'sardine', 'chimpanzee', 'eland', 'wolf', 'fly', 'quelea', 'ferret', 'lyrebird', 'barracuda', 'cat', 'manatee', 'reindeer', 'okapi', 'panther', 'anteater', 'camel', 'swallow', 'gazelle', 'porcupine', 'crocodile', 'coyote', 'whale', 'chamois', 'elephant', 'gaur', 'hyena', 'hamster', 'moose', 'goshawk', 'shrew', 'narwhal', 'gnat', 'leopard', 'lion', 'octopus', 'dotterel', 'viper', 'bee', 'dogfish', 'quail', 'porpoise', 'hippopotamus', 'turkey', 'raven', 'badger', 'fox', 'penguin', 'worm', 'human', 'tiger', 'salamander', 'swan', 'oryx', 'kudu', 'termite', 'gull', 'gorilla', 'newt', 'seahorse', 'mandrill', 'rail', 'kouprey', 'starling', 'pony', 'locust', 'shark', 'frog', 'wren', 'alpaca', 'pheasant', 'crane', 'loris', 'tapir', 'bat', 'baboon', 'giraffe', 'mongoose', 'seal', 'kingfisher', 'finch', 'lobster', 'skunk', 'vulture', 'zebra', 'iguana', 'bird', 'cheetah', 'stingray', 'armadillo', 'heron', 'aardvark', 'tarsier', 'buffalo', 'mantis', 'elk', 'hummingbird', 'raccoon', 'opossum', 'snail', 'toad', 'partridge', 'rook', 'dinosaur', 'donkey', 'herring', 'wolverine', 'duck', 'chinchilla', 'snake', 'cattle', 'curlew', 'ostrich', 'dragonfly', 'hare', 'bison', 'magpie', 'boar', 'emu', 'goldfinch', 'otter', 'parrot', 'wombat', 'scorpion', 'dog', 'gerbil', 'cormorant', 'cod', 'cobra', 'lark', 'mosquito', 'pig', 'red panda', 'sparrow', 'dolphin', 'gnu', 'falcon', 'hornet', 'koala', 'quetzal', 'eagle', 'mouse', 'fish', 'lemur', 'rabbit', 'trout', 'kookabura', 'stinkbug', 'bear', 'mule', 'rhinoceros', 'oyster', 'owl', 'red deer', 'hawk', 'sheep', 'stork', 'echidna', 'marten', 'rat', 'capybara', 'butterfly', 'chough', 'flamingo', 'jaguar', 'woodcock', 'caterpillar', 'guanaco', 'pelican', 'squirrel', 'salmon', 'nightingale', 'kangaroo', 'eel', 'spoonbill'}
     if animal in list_of_animals:
@@ -216,15 +192,12 @@ def find_suggestion(intent_request):
     animal= get_slots(intent_request)["animal"]
     genre= get_slots(intent_request)["genre"]
     source = intent_request['invocationSource']
-    
-    
     print(source)
     
     if source == 'DialogCodeHook':
         # Perform basic validation on the supplied input slots.
         # Use the elicitSlot dialog action to re-prompt for the first violation detected.
         slots = get_slots(intent_request)
-
         validation_result = validate_rec_request(fav_color, animal, genre)
         print("LUID", LEXUSERID, "<-")
         
@@ -232,8 +205,6 @@ def find_suggestion(intent_request):
         if intent_request['sessionAttributes'] is None:
             intent_request['sessionAttributes'] = {}
             intent_request['sessionAttributes']["LEXuserid"] = LEXUSERID
-        
-
         if not validation_result['isValid']: # Validation failed
             slots[validation_result['violatedSlot']] = None
             return elicit_slot(intent_request['sessionAttributes'],
@@ -241,12 +212,7 @@ def find_suggestion(intent_request):
                                slots,
                                validation_result['violatedSlot'],
                                validation_result['message'])
-
-        # Pass the price of the flowers back through session attributes to be used in various prompts defined
-        # on the bot model.
-        output_session_attributes = intent_request['sessionAttributes'] if intent_request[
-                                                                               'sessionAttributes'] is not None else {}
-
+        output_session_attributes = intent_request['sessionAttributes'] if intent_request['sessionAttributes'] is not None else {}
         item_list = fav_color, animal, genre
 
         allIsGood = True
@@ -263,10 +229,8 @@ def find_suggestion(intent_request):
     # Trigger lambda and pass animal, fav color, genre, LEXID to new lambda (don't wait for response) 
     # query database with LEXID
     
-    ###### CALL NEXT LAMBDA HERE (comment out everything below)
     data = str(LEXUSERID)+ " " + str(animal)+ " " + str(fav_color)+ " " + str(genre)
     resp = send_to_next_lambda(data)
-    ######
 
     return close(intent_request['sessionAttributes'],
                  'Fulfilled',
@@ -274,9 +238,7 @@ def find_suggestion(intent_request):
                   'content': "Your recommendation will be posted to your profile!"})
 
 
-
 """ --- Intents --- """
-
 def dispatch(intent_request):
     """
     Called when the user specifies an intent for this bot.
@@ -296,13 +258,10 @@ def dispatch(intent_request):
     raise Exception('Intent with name ' + intent_name + ' not supported')
 
 
-
 def queue_test():
-    # tests queue
     vector = "0 0 255 101 101 101"
     genre = "hiphop"
     username = "testUser"
-    
     mbody = "Client recommendation request" + str(random.random())
     attr = {
         'vector': {
@@ -323,18 +282,14 @@ def queue_test():
     return response
 
 
-
 """ --- Main handler --- """
 def lambda_handler(event, context):
     print("EVENT AT HANDLER:", event)
     print("CONTEXT AT HANDLER:", context)
-    
     uid = event['userId']
     print("GETTING USERId:", uid)
     global LEXUSERID
     LEXUSERID = uid
-    
-     
     """
     Route the incoming request based on intent.
     The JSON body of the request is provided in the event slot.
